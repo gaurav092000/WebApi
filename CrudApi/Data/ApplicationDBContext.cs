fb176cdd57a7a266962af2e240d6caebf4041a6a
Nimap <Nimap@DESKTOP-KK6MUO2>
2023-09-11 19:42:27 +0530

    Add project files.

---
+++ CrudApi/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CrudApi.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext() : base("WebApiCrud") { }
+
+
+    }
+}
---
+++ WebApi/Controllers/EmployeesDataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class EmployeesDataController : ApiController
+    {
+        public string[] myEmpoyees = { "Gaurav", "Raju", "Ramesh" };
+        [HttpGet]
+        public string[] GetEmployees()
+        {
+            return myEmpoyees;
+        }
+
+        [HttpGet]
+        public string GetEmplyeesByIndex(int id)
+        {
+            return myEmpoyees[id];
+        }
+    }
+}
